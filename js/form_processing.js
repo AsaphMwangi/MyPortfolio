d36f1630b83d889d6567f3e82682637d50b5746f
Asaph MWangi <asaphmwangi1973@gmail.com>
2025-02-27 20:43:26 +0300

    update

---
+++ js/form_processing.js
@@ -0,0 +1,13 @@
+//Form details
+
+const loading_gif = document.getElementById("loading");
+
+const submit_btn = document.getElementById("submit");
+
+
+//Processing of data submitted
+submit_btn.addEventListener("click", () =>
+{
+  loading_gif.style.display = "inline";
+  
+});
---
+++ js/sidebar_processing.js
@@ -0,0 +1,25 @@
+//Function to open and close the sidebar
+        const sidebar = document.getElementById("sidebar_container"); 
+        const menu_button = document.getElementById("menu_btn");
+        const close_button = document.getElementById("close_btn");       
+        const side_bar = document.getElementById("sidebar_container");
+        const device_width = window.innerWidth;
+        menu_button.addEventListener("click", () =>{
+            
+            side_bar.style.display = "block";
+            menu_button.style.display = "none";
+            close_button.style.display = "block"
+        });
+        close_button.addEventListener("click", () =>{
+            
+            side_bar.style.display = "none";
+            menu_button.style.display = "block";
+            close_button.style.display = "none"
+        });
+
+        if(device_width >= 519)
+        {
+            side_bar.style.display = "none";
+            menu_button.style.display = "none";
+            close_button.style.display = "none"
+        }
