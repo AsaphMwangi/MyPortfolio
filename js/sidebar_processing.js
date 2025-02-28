7bd0d5f822a37e60f06730f34adfa29fd295d5c7
Asaph MWangi <asaphmwangi1973@gmail.com>
2025-02-28 10:13:08 +0300

    Update

--- js/sidebar_processing.js
+++ js/sidebar_processing.js
@@ -4,6 +4,8 @@
         const close_button = document.getElementById("close_btn");       
         const side_bar = document.getElementById("sidebar_container");
         const device_width = window.innerWidth;
+        const paragraph_container = document.getElementById("intro_paragraph_container");
+
         menu_button.addEventListener("click", () =>{
             
             side_bar.style.display = "block";
@@ -16,5 +18,15 @@
             menu_button.style.display = "block";
             close_button.style.display = "none"
         });
+        
+        paragraph_container.addEventListener("click", ()=>{
+            if(!side_bar.contains(event.target))
+                {
+                    side_bar.style.display = "none";
+                    menu_button.style.display = "block";
+                    close_button.style.display = "none"
+                }
+        });
+        
 
         
